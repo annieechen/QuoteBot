62449216639e7505935874b5635e1d31b08e33f6
Annie Chen <annie.chen@yale.edu>
2016-07-15 01:54:18 +0000

    adding commenting and styling to first half of index.js

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-/*var express = require('express');
-var app = express();
-var url = require('url');
-var request = require('request');
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('port', (process.env.PORT || 9001));
-
-app.get('/', function(req, res){
-  res.send('It works!');
-});
-
-app.post('/post', function(req, res){
-  var temp = ["I am asleep", "perhaps I am awake", "I'm so cute!", "I'm hungry"];
-  var body = {
-    response_type: "in_channel",
-    text: temp[Math.floor((Math.random() * 4) + 1)]
-  };
-
-  res.send(body);
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-*/
-
+// dependencies
 var express = require('express');
 var app = express();
 var url = require('url');
@@ -36,38 +8,20 @@ var request = require('request');
 var mongodb = require('mongodb');
 var assert = require('assert');
 
-// var quotelist;
-// try {
-//   quotelist = JSON.parse(fs.readFileSync('quotelist.json').toString());
-// }
-// catch (e) {
-//   quotelist = [];
-//   fs.writeFile('quotelist.json', '[]');
-// }
-
+// initialize mongoose and connect
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://elphaba:elphaba@ds047802.mlab.com:47802/quotelist');
 
 var db = mongoose.connection;
 
+// create schema reference
 var quoteschema = mongoose.Schema({
     name      : String,
     quote     : String
 });
 
+// compile schema into model
 var Quote = mongoose.model('quotes', quoteschema);
-
-var oldquote = new Quote({ name: "Brian", quote:"Not throwing away my shot"});
-    console.log(oldquote.name);
-    // save potato to database
-    oldquote.save(function(err, oldquote) {
-      if (err) return console.error(err);
-    });
-
-Quote.find({"name" : "Annie"}, function (err, kittens) {
-  if (err) return console.error(err);
-  console.log(kittens);
-});
 
 
 var bodyParser = require('body-parser');
@@ -76,34 +30,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 9001));
 
+// this shows up on the deployed heroku webstite
 app.get('/', function(req, res){
   res.send('It works!');
 });
 
 app.post('/post', function(req, res){
   var command = req.body.text;
-  
+  // if second word is add, let's add a quote!
   if (command.match(/^add/) != null) {
     var name = command.split(' ')[1].toLowerCase();
     var quote = command.split(' ').slice(2).join(' ');
-    // quotelist.push({
-    //   'name': name,
-    //   'quote': quote
-    // });
-    // fs.writeFile('quotelist.json', JSON.stringify(quotelist));
+    
+    // create new quote document
     var newquote = new Quote({ name: name, quote: quote});
     console.log(newquote.name);
-    // save potato to database
+    
+    // save new quote to database
     newquote.save(function(err, newquote) {
       if (err) return console.error(err);
     });
+    
+    // create string that slackbot prints in channel
     var body = {
       response_type: "in_channel",
       text: name[0].toUpperCase() + name.slice(1).toLowerCase() + "'s quote has been added!"
     };
+    // send to channel, in response to /quotelist add <name> <quote>
     res.send(body);
   }
-  // looking for an existing quote
+  // looking for a person's existing quotes
   else {
     var name = command.toLowerCase();
     // var quotes = quotelist.filter(function(x) { return (x.name == name); })
